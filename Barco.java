bb9777d4ce76764b5306e1ad17470d013ffc0631
george2 <emelyrivera6305@gmail.com>
2026-05-28 19:59:34 -0600

    Añadir la clase Barco con lógica de impactos y hundimiento

--- Barco.java
+++ Barco.java
@@ -1,5 +1,50 @@
 package batallanaval;
 
 public class Barco {
+	 private String name;
+	    private int size;
+	    private int hits;
+	    private boolean horizontal;
+	    private int startRow;
+	    private int startCol;
 
-}
+	    public Barco(String name, int size) {
+	        this.name = name;
+	        this.size = size;
+	        this.hits = 0;
+	        this.horizontal = true;
+	        this.startRow = -1;
+	        this.startCol = -1;
+	    }
+
+	    public String getName() { return name; }
+	    public int getSize() { return size; }
+	    public int getHits() { return hits; }
+	    public boolean isHorizontal() { return horizontal; }
+	    public int getStartRow() { return startRow; }
+	    public int getStartCol() { return startCol; }
+
+	    public void setHorizontal(boolean horizontal) { this.horizontal = horizontal; }
+	    public void setStartRow(int startRow) { this.startRow = startRow; }
+	    public void setStartCol(int startCol) { this.startCol = startCol; }
+
+	    
+	    public boolean hit() {
+	        hits++;
+	        return isSunk();
+	    }
+
+	    
+	    public boolean isSunk() {
+	        return hits >= size;
+	    }
+
+	    
+	    public String toString() {
+	        return name + " (" + size + " espacios)";
+	    }
+	}
+	    
+	
+
+
